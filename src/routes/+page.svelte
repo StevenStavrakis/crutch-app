565343b732910962be76375462550a329ec94b2a
Steven Stavrakis <s.a.stavrakis@gmail.com>
2024-03-23 16:09:21 -0400

    mapbox added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+	import { mapboxgl } from '$lib/map';
+	let map: mapboxgl.Map | undefined = $state();
+	$effect(() => {
+		map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11'
+		});
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen w-screen" id="map"></div>
